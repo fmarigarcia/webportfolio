bb763f55315ce767f071fab175790a69ae7432a3
Fran Marí <fran.mari.94@gmail.com>
2023-05-19 22:11:35 +0200

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { ServerThemeProvider } from 'next-themes';
-import './styles/globals.css';
-import ClientThemeProvider from './providers/ClientThemeProvider';
 import { Quicksand } from 'next/font/google';
 import clsx from 'clsx';
+import ClientThemeProvider from './providers/ClientThemeProvider';
+import './styles/globals.css';
 
 export const metadata = {
   title: 'WebPortfolio',
@@ -14,18 +13,16 @@ const quicksand = Quicksand({ subsets: ['latin-ext'] });
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <ServerThemeProvider attribute="class">
-      <html>
-        <body
-          className={clsx(
-            quicksand.className,
-            'h-screen bg-background-light text-text-light dark:bg-background-dark dark:text-text-dark'
-          )}
-        >
-          <ClientThemeProvider>{children}</ClientThemeProvider>
-        </body>
-      </html>
-    </ServerThemeProvider>
+    <html>
+      <body
+        className={clsx(
+          quicksand.className,
+          'h-screen bg-background-light text-text-light dark:bg-background-dark dark:text-text-dark'
+        )}
+      >
+        <ClientThemeProvider>{children}</ClientThemeProvider>
+      </body>
+    </html>
   );
 };
 
