88720c1dba0dd96f11e6b42fc70795a60e404116
Fran Marí <fran.mari.94@gmail.com>
2023-05-19 22:18:35 +0200

    test

---
+++ app/layout copy.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Quicksand } from 'next/font/google';
+import clsx from 'clsx';
+import ClientThemeProvider from './providers/ClientThemeProvider';
+import './styles/globals.css';
+
+export const metadata = {
+  title: 'WebPortfolio',
+  description: 'Francisco Marí García web portfolio'
+};
+
+const quicksand = Quicksand({ subsets: ['latin-ext'] });
+
+const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <html>
+      <body
+        className={clsx(
+          quicksand.className,
+          'h-screen bg-background-light text-text-light dark:bg-background-dark dark:text-text-dark'
+        )}
+      >
+        <ClientThemeProvider>{children}</ClientThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Quicksand } from 'next/font/google';
-import clsx from 'clsx';
-import ClientThemeProvider from './providers/ClientThemeProvider';
 import './styles/globals.css';
 
 export const metadata = {
@@ -9,19 +6,10 @@ export const metadata = {
   description: 'Francisco Marí García web portfolio'
 };
 
-const quicksand = Quicksand({ subsets: ['latin-ext'] });
-
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html>
-      <body
-        className={clsx(
-          quicksand.className,
-          'h-screen bg-background-light text-text-light dark:bg-background-dark dark:text-text-dark'
-        )}
-      >
-        <ClientThemeProvider>{children}</ClientThemeProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 };
