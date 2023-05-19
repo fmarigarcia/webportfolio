59b13dbecf44d7a19e95aa5985cb502afdd0c3d1
Fran Marí <fran.mari.94@gmail.com>
2023-05-19 22:28:27 +0200

    test

---
+++ next.config copy.js
@@ -0,0 +1,5 @@
+/** @type {import('next').NextConfig} */
+const nextTranslate = require('next-translate-plugin');
+const nextConfig = nextTranslate({ experimental: { appDir: true } });
+
+module.exports = nextConfig;
--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextTranslate = require('next-translate-plugin');
-const nextConfig = nextTranslate({ experimental: { appDir: true } });
+const nextConfig = { experimental: { appDir: true } };
 
 module.exports = nextConfig;
