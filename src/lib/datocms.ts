c6a1c4ddb7408862b928b9d86840b6be691e8e82
Fran Marí <fran.mari.94@gmail.com>
2024-02-04 16:15:43 +0100

    more changes

--- src/lib/datocms.ts
+++ src/lib/datocms.ts
@@ -7,18 +7,16 @@ interface IRequestProps {
   includeDrafts?: boolean;
   excludeInvalid?: boolean;
 }
-export const request = ({ query, variables, includeDrafts = false, excludeInvalid = false }: IRequestProps): any => {
-  const token = process.env.NEXT_PUBLIC_NEXT_DATOCMS_API_TOKEN;
-  if (token === undefined) return;
-  const headers: GraphQLClientRequestHeaders = {
-    authorization: `Bearer ${token}`
-  };
-  if (includeDrafts) {
-    headers['X-Include-Drafts'] = 'true';
-  }
-  if (excludeInvalid) {
-    headers['X-Exclude-Invalid'] = 'true';
-  }
-  const client = new GraphQLClient('https://graphql.datocms.com', { headers });
+
+const token = process.env.NEXT_PUBLIC_NEXT_DATOCMS_API_TOKEN;
+const headers: GraphQLClientRequestHeaders = {
+  authorization: `Bearer ${token as string}`
+};
+
+const client = new GraphQLClient('https://graphql.datocms.com', { headers });
+
+const request = ({ query, variables }: IRequestProps): any => {
   return client.request(query, variables);
 };
+
+export default request;
