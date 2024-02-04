6c0c8e24a85101adadda914b09ab7990c4814021
Fran Marí <fran.mari.94@gmail.com>
2024-02-04 16:19:35 +0100

    revert

--- src/lib/datocms.ts
+++ src/lib/datocms.ts
@@ -7,15 +7,19 @@ interface IRequestProps {
   includeDrafts?: boolean;
   excludeInvalid?: boolean;
 }
-
-const token = process.env.NEXT_PUBLIC_NEXT_DATOCMS_API_TOKEN;
-const headers: GraphQLClientRequestHeaders = {
-  authorization: `Bearer ${token as string}`
-};
-
-const client = new GraphQLClient('https://graphql.datocms.com', { headers });
-
-const request = ({ query, variables }: IRequestProps): any => {
+const request = ({ query, variables, includeDrafts = false, excludeInvalid = false }: IRequestProps): any => {
+  const token = process.env.NEXT_PUBLIC_NEXT_DATOCMS_API_TOKEN;
+  if (token === undefined) return;
+  const headers: GraphQLClientRequestHeaders = {
+    authorization: `Bearer ${token}`
+  };
+  if (includeDrafts) {
+    headers['X-Include-Drafts'] = 'true';
+  }
+  if (excludeInvalid) {
+    headers['X-Exclude-Invalid'] = 'true';
+  }
+  const client = new GraphQLClient('https://graphql.datocms.com', { headers });
   return client.request(query, variables);
 };
 
